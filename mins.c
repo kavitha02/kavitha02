b9a94c3856acfc723f6248b9e74458d0946edd49
kavitha02 <karpaga.cs13@bitsathy.ac.in>
2016-06-08 11:09:01 +0530

    Create mins.c

---
+++ mins.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int h,m,tm,h1,m1,tm1,lm;
+scanf("%d%d",&h,&m);
+tm=h*60;
+tm=tm+m;
+scanf("%d%d",&h1,&m1);
+tm1=h1*60;
+tm1=tm1+m1;
+lm=tm1-tm;
+printf("%d",leftmin);
+return 0;
+
+}
