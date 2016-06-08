d8fa2baccb2a205dd85b084d430101bad14480c0
kavitha02 <karpaga.cs13@bitsathy.ac.in>
2016-06-08 10:57:25 +0530

    Create isomorp.py

---
+++ isomorp.py
@@ -0,0 +1,9 @@
+def isIsomorphic(self, s, t):
+        m1 = [0]*256
+        m2 = [0]*256
+        for i in range(len(s)):
+            if m1[ord(s[i])] != m2[ord(t[i])]:
+                return False
+            m1[ord(s[i])] = i+1
+            m2[ord(t[i])] = i+1
+        return True
